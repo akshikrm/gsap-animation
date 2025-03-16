5660380d372f1e19add4ede8f4f6c0ff3a8b8070
Akshay Krishna <akshikrm@gmail.com>
2025-03-16 22:27:16 +0530

    batman

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,73 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import gsap from "gsap";
+import { useEffect } from "react";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    gsap.from(".animated-text", {
+      scale: 1,
+      opacity: 1,
+      fontSize: "100px",
+      scrollTrigger: {
+        trigger: ".text-wrapper",
+        start: "top top",
+        end: "80% top",
+        scrub: true,
+        markers: true,
+        pin: ".text-wrapper",
+      },
+    });
+
+    gsap.from(".animated-image", {
+      scale: 1,
+      height: "100vh",
+      filter: "blur(0px)",
+      scrollTrigger: {
+        trigger: ".animated-text",
+        // toggleActions: "play none reverse none",
+        markers: true,
+        scrub: true,
+        start: "-300px top",
+      },
+    });
+  }, []);
+
+  return (
+    <>
+      <div className="text-wrapper">
+        <div className="animated-image-foreground" />
+        <div className="animated-image" />
+        <h1 className="animated-text">Scale-Up Effect</h1>
+      </div>
+    </>
   );
 }
+//
+//
+// Function Code:
+//     ---------------
+// gsap.from(animatedText, {
+//   scrollTrigger: {
+//     trigger: animatedText,
+//     start: "top 90%",
+//     end: "top 40%",
+//     scrub: true,
+//   },
+//   scale: 0.5,
+//   opacity: 0,
+//   duration: 2,
+// });
+//
+//     HTML Code:
+//     ---------------
+//     <section class="container-fluid vh-100 d-flex align-items-center justify-content-center position-relative effect-section" data-effect="scale-up">
+//           <button class="btn btn-light position-absolute top-0 end-0 m-3 copy-btn">Copy Code</button>
+//           <div class="row w-100">
+//             <div class="col-12 text-center">
+//               <h1 class="animated-text" style="translate: none; rotate: none; scale: none; opacity: 0.4643; transform: translate3d(0px, 0px, 0px) scale(0.7322);">Scale-Up Effect</h1>
+//             </div>
+//           </div>
+//         </section>
